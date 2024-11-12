c57253589abc5cda3935da12af1b0411268a2b6c
iashishhh <ashish1862ac@gmail.com>
2024-11-12 12:52:27 +0000

    push

---
+++ search.js/slickSlider.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        centerPadding: '14',
+        autoplay: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    dots: true,
+                    slidesToShow: 2,
+                    // slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    dots: true,
+                    slidesToShow: 2,
+                    // slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
