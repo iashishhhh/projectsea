6e7cbd30b05091695a22f3f7382867c5d550ceae
iashishhh <ashish1862ac@gmail.com>
2024-11-20 10:33:18 +0000

    push

--- search.js/slickSlider.js
+++ search.js/slickSlider.js
@@ -34,3 +34,30 @@ $(document).ready(function () {
         ]
     });
 });
+
+
+
+$(document).ready(function () {
+    const toggler = $('.navbar-toggler');
+    const navbarCollapse = $('#navbarSupportedContent');
+
+    // Toggle the class for the cross icon
+    toggler.on('click', function () {
+        $(this).toggleClass('toggler-open'); // Add/remove the toggler-open class
+    });
+
+    // Optional: Close the navbar and reset the icon when clicking outside
+    $(document).on('click', function (event) {
+        if (!navbarCollapse.is(event.target) && !navbarCollapse.has(event.target).length &&
+            !toggler.is(event.target) && !toggler.has(event.target).length) {
+            navbarCollapse.removeClass('show');
+            toggler.removeClass('toggler-open'); // Reset the icon
+
+        }
+
+    });
+});
+
+
+
+
