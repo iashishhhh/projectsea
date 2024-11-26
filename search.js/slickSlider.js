7d23be585e2cebad0b61bc1dc11b5e125b20e12e
iashishhh <ashish1862ac@gmail.com>
2024-11-26 13:26:55 +0000

    add

--- search.js/slickSlider.js
+++ search.js/slickSlider.js
@@ -35,8 +35,6 @@ $(document).ready(function () {
     });
 });
 
-
-
 // open and close toggle 
 $(document).ready(function () {
     // Define Variables for Navbar Elements --1
@@ -46,7 +44,7 @@ $(document).ready(function () {
     navbarCollapse.on('shown.bs.collapse', function () {
         toggler.addClass('toggler-open'); // Show open icon
     });
-    // close navbar
+    //close navbar
     navbarCollapse.on('hidden.bs.collapse', function () {
         toggler.removeClass('toggler-open'); // Reset to close icon
     });
@@ -61,4 +59,64 @@ $(document).ready(function () {
 });
 
 
+//js for screen smooth scrolling
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
 
+
+
+// Ajax form submit ...................................
+// Intercepting the form submission
+$("#ajaxForm").submit(function (e) {
+    e.preventDefault(); // Default form submission prevent karein
+
+    // Getting the form's action attribute
+    var action = $(this).attr("action") || "/default-url"; // Agar action attribute nahi ho, default URL ka use karein
+    console.log("Action URL:", action);
+
+    // Serializing form data
+    var data = {};
+    $(this).serializeArray().forEach(function (field) {
+        data[field.name] = field.value;
+    });
+    console.log("Serialized Data:", data);
+
+    // Sending AJAX POST request
+    $.ajax({
+        type: "POST",
+        // url: action,
+        data: JSON.stringify(data), // Sending serialized data as JSON
+        contentType: "application/json", // JSON data content type
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+        .done(function (response) {
+            console.log("Form submitted successfully!", response);
+            alert("Form submitted successfully!");
+            $("#ajaxForm")[0].reset(); // Reset form fields
+        })
+        .fail(function (xhr, status, error) {
+            console.error("Error: ", status, error);
+            alert("Error: Form submission failed. Please try again later.");
+        });
+});
+
+// code input clear field
+document.getElementById("ajaxForm").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    var form = event.target;
+    var inputs = form.querySelectorAll("input, textarea");
+
+    inputs.forEach(function (input) {
+        input.value = "";
+    });
+
+});
